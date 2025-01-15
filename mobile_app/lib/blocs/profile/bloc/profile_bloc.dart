d39fb869de5826717d1811e8a9744c01a0070633
mpilipov <misha7538@live.ru>
2025-01-15 23:44:18 +0100

    Update profile_bloc.dart

--- mobile_app/lib/blocs/profile/bloc/profile_bloc.dart
+++ mobile_app/lib/blocs/profile/bloc/profile_bloc.dart
@@ -2,7 +2,7 @@ import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:study_buds/network/request/profile_request.dart';
 import 'package:study_buds/network/request/update_telegram_account.dart';
-
+import 'package:hive/hive.dart';
 import '../../../models/student.dart';
 
 part 'profile_event.dart';
@@ -18,10 +18,28 @@ class ProfileBloc extends Bloc<ProfileEvent, ProfileState> {
       FetchProfileDetailsEvent event, Emitter<ProfileState> emit) async {
     try {
       emit(ProfileLoading());
+
+      // hive caching
+      // open the Hive box
+      final box = Hive.box<Student>('profiles');
+
+      // check if data is cached
+      final cachedStudent = box.get('user_profile');
+      if (cachedStudent != null) {
+        emit(ProfileLoaded(cachedStudent));
+        return;
+      }
       final request = ProfileRequest();
       final response = await request.send();
 
       if (response.isSuccess) {
+        //caching 
+        final student = Student.fromJson(response.data);
+
+        // cache the fetched data
+        await box.put('user_profile', student);
+        //end caching
+
         emit(ProfileLoaded(response.data));
       } else {
         emit(ProfileError(error: 'Failed to get profile information'));
@@ -38,6 +56,14 @@ class ProfileBloc extends Bloc<ProfileEvent, ProfileState> {
       final request = UpdateTelegramAccountRequest(event.telegramAccountId);
       final response = await request.send();
       if (response.isSuccess) {
+        //caching
+        final student = Student.fromJson(response.data);
+
+        // update the cached data
+        final box = Hive.box<Student>('profiles');
+        await box.put('user_profile', student);
+        //end caching
+
         emit(ProfileSaveSuccess(response.data));
       } else {
         emit(ProfileSaveFailed(error: 'Failed to update telegram account'));
